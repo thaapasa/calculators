11fb30fe7301e1e0c305b39c06baf250484a043e
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-10-31 21:15:16 +0200

    Add grunt-browserify for building JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,24 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
+        src: 'build/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    browserify: {
+      client: {
+        src: 'src/js/*.js',
+        dest: 'build/<%= pkg.name %>.js'
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Load the plugin that provides the "browserify" task.
+  grunt.loadNpmTasks('grunt-browserify');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['browserify']);
 
 };
