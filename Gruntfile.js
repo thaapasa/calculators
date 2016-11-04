779fee2d1d533885d357a00f09d18df5e131a931
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-04 22:45:32 +0200

    Added source file watching for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,43 @@
+"use strict";
+
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'public/js/calculators.js',
-        dest: 'public/js/calculators.min.js'
-      }
-    },
-    browserify: {
-      options: {
-        transform: [
-          ["babelify", {
-            presets: ["es2015"]
-          }]
-        ]
-      },
-      client: {
-        src: 'src/app/main.js',
-        dest: 'public/js/calculators.js'
-      }
-    },
-    watchify: {
-      options: {
-        detectGlobals: true,
-        insertGlobals: false,
-        ignoreMissing: false,
-        debug: false,
-        standalone: false,
-        keepalive: true,
-        transform: [
-          ["babelify", {
-            presets: ["es2015"]
-          }]
-        ]
-      },
-      client: {
-        src: './src/app/main.js',
-        dest: 'public/js/calculators.js'
-      }
-    }
-  });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-watchify');
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: './public/js/calculators.js',
+                dest: './public/js/calculators.min.js'
+            }
+        },
+        browserify: {
+            options: {
+                transform: [
+                    ["babelify", { presets: ["es2015"] }]
+                ]
+            },
+            client: {
+                src: './src/app/main.js',
+                dest: './public/js/calculators.js'
+            }
+        },
+        watch: {
+            files: ["./src/app/**/*.js"],
+            tasks: ["build"]
+        }
+    });
 
-  // Default task(s).
-  grunt.registerTask('default', ['browserify']);
+    grunt.registerTask("build", ["browserify:client"]);
+    grunt.registerTask("dev", ["build", "watch"]);
+
+    grunt.registerTask("default", ["build"]);
 
 };
