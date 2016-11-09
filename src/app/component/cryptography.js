090fe6efbdfa7f90b1d049f6bed5b715f01f36fc
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-09 22:14:53 +0200

    Refactored cryptography output into its own component

--- src/app/component/cryptography.js
+++ src/app/component/cryptography.js
@@ -1,48 +1,14 @@
-const $ = require("jquery")
 import React from 'react'
-import * as Bacon from "baconjs"
 import * as BaconUtil from "../util/baconutil"
-import * as util from "../util/util"
+import SelectableOutput from "./selectable-output"
 import {hex_md5} from "../calc/md5"
 import {sha1} from "../calc/sha1"
 
 export default class Cryptography extends React.Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            md5Value: "",
-            sha1Value: ""
-        }
-    }
-
     componentDidMount() {
-        const cryptoSelect = BaconUtil.getRadioStream("crypto", "md5")
-        const inputStream = $("#plain-text-input").asEventStream("keyup").map(BaconUtil.eventToValue)
-
-        Bacon.mergeAll(
-            this.streamCalculation(inputStream, hex_md5, this.ucIfChecked($("#md5-uc")), "md5Value", cryptoSelect.map((v) => v == "md5")),
-            this.streamCalculation(inputStream, sha1, this.ucIfChecked($("#sha1-uc")), "sha1Value", cryptoSelect.map((v) => v == "sha1")))
-            .onValue(this.props.onValue)
-    }
-
-    streamCalculation(inputStream, calculation, calcMapper, valueName, valueFilter) {
-        let calculated = inputStream.map(calculation)
-        if (calcMapper) {
-            calculated = calculated.combine(calcMapper, (val, m) => m(val))
-        }
-        calculated.onValue((value) => this.setState( { [valueName]: value } ))
-
-        return valueFilter ?
-            Bacon.combineAsArray(calculated, valueFilter)
-                .filter((t) => t[1])
-                .map((t) => t[0]) :
-            calculated
-    }
-
-    ucIfChecked(element) {
-        return BaconUtil.getCheckboxStream(element).map((checked) => checked ? util.toUpperCase : util.identity)
+        const inputStream = BaconUtil.textFieldValue("#plain-text-input")
+        inputStream.onValue(v => [this.refs.md5, this.refs.sha1].forEach(r => r.setValue(v)))
     }
 
     render() {
@@ -54,31 +20,8 @@ export default class Cryptography extends React.Component {
                     <textarea id="plain-text-input" className="large" />
                 </div>
             </div>
-            <div className="calculator item">
-                <div className="name">
-                    <input type="radio" name="crypto" value="md5" defaultChecked id="crypto-select-md5" /><label htmlFor="crypto-select-md5">MD5</label>
-                    <div className="tools">
-                        <input type="checkbox" id="md5-uc" />
-                        <label htmlFor="md5-uc" title="Isot kirjaimet"><i className="fa fa-font" /></label>
-                    </div>
-                </div>
-                <div className="value">
-                    <input type="text" id="md5-text" className="wide" value={this.state.md5Value} readOnly />
-                </div>
-            </div>
-            <div className="calculator item">
-                <div className="name">
-                    <input type="radio" name="crypto" value="sha1" id="crypto-select-sha1" /><label htmlFor="crypto-select-sha1">SHA-1</label>
-                    <div className="tools">
-                        <input type="checkbox" id="sha1-uc" />
-                        <label htmlFor="sha1-uc" title="Isot kirjaimet"><i className="fa fa-font" /></label>
-                    </div>
-                </div>
-                <div className="value">
-                    <input type="text" id="sha1-text" className="wide" value={this.state.sha1Value} readOnly />
-                </div>
-            </div>
+            <SelectableOutput ref="md5" id="md5" group="crypto" label="MD5" calculate={hex_md5} onValue={this.props.onValue} default={true} />
+            <SelectableOutput ref="sha1" id="sha1" group="crypto" label="SHA-1" calculate={sha1} onValue={this.props.onValue} />
         </section>
-
     }
 }
---
+++ src/app/component/selectable-output.js
@@ -0,0 +1,62 @@
+const $ = require("jquery")
+import React from 'react'
+import * as Bacon from "baconjs"
+import * as BaconUtil from "../util/baconutil"
+import * as util from '../util/util'
+
+export default class SelectableOutput extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.setValue = this.setValue.bind(this)
+        this.state = { value: "" };
+    }
+
+    componentDidMount() {
+        const cryptoSelect = BaconUtil.getRadioStream(this.props.group, this.props.name)
+        const inputStream = new Bacon.Bus()
+        this.setState({ inputStream: inputStream })
+        this.streamCalculation(inputStream, this.props.calculate, this.ucIfChecked($(`#uc-${this.props.id}`)), cryptoSelect.map((v) => v == this.props.id))
+    }
+
+    setValue(val) {
+        this.state.inputStream.push(val)
+    }
+
+    streamCalculation(inputStream, calculation, calcMapper, valueFilter) {
+        let calculated = inputStream.map(calculation)
+        if (calcMapper) {
+            calculated = calculated.combine(calcMapper, (val, m) => m(val))
+        }
+        calculated.onValue((value) => this.setState( { value: value } ))
+
+        const valueStream = (valueFilter ?
+            Bacon.combineAsArray(calculated, valueFilter)
+                .filter((t) => t[1])
+                .map((t) => t[0]) :
+            calculated)
+        valueStream.onValue(this.props.onValue)
+    }
+
+    ucIfChecked(element) {
+        return BaconUtil.getCheckboxStream(element).map((checked) => checked ? util.toUpperCase : util.identity)
+    }
+
+    render() {
+        const radioButton = this.props.default ?
+            <input type="radio" name={this.props.group} value={this.props.id} id={`crypto-select-${this.props.id}`} defaultChecked /> :
+            <input type="radio" name={this.props.group} value={this.props.id} id={`crypto-select-${this.props.id}`} />
+        return <div className="calculator item">
+            <div className="name">
+                {radioButton}<label htmlFor={`crypto-select-${this.props.id}`}>{ this.props.label }</label>
+                <div className="tools">
+                    <input type="checkbox" id={`uc-${this.props.id}`} />
+                    <label htmlFor={`uc-${this.props.id}`} title="Isot kirjaimet"><i className="fa fa-font" /></label>
+                </div>
+            </div>
+            <div className="value">
+                <input type="text" id={`${this.props.id}-text`} className="wide" value={this.state.value} readOnly />
+            </div>
+        </div>
+    }
+}
