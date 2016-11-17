28a6161154a88dc62f89812194bfee81e613026f
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-17 22:16:45 +0200

    Added Material Design libraries

--- src/app/main.js
+++ src/app/main.js
@@ -5,16 +5,25 @@ import ReactDOM from "react-dom"
 import CalculatorPage from "./ui/page"
 import * as util from "./util/util"
 import * as BaconUtil from "./util/baconutil"
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 function init() {
+    // Ensure that there is a console
     util.fixConsole()
+    // Needed for onTouchTap
+    // http://stackoverflow.com/a/34015469/988941
+    injectTapEventPlugin()
+
     BaconUtil.addBaconSafeLog()
     BaconUtil.attachToJQuery()
     renderCalculators()
 }
 
 function renderCalculators() {
-    ReactDOM.render(<CalculatorPage />, document.getElementById("root"))
+    ReactDOM.render(<MuiThemeProvider>
+        <CalculatorPage />
+    </MuiThemeProvider>, document.getElementById("root"))
 }
 
 document.addEventListener("DOMContentLoaded", init)
