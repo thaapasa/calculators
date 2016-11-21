95f976bc418c41797045ea6a75a9c543065dea13
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-21 21:40:35 +0200

    Propagated selected number value to UI last value field

--- src/app/ui/numbers.js
+++ src/app/ui/numbers.js
@@ -6,22 +6,31 @@ import * as numbers from "../calc/numbers"
 import * as util from "../util/util"
 import TextField from 'material-ui/TextField';
 
+const texts = {
+    "binary": "Binääri",
+    "octal": "Oktaali",
+    "decimal": "Desimaali",
+    "hex": "Heksa"
+}
+
 export default class Numbers extends React.Component {
 
     constructor(props) {
         super(props)
         this.inputChanged = this.inputChanged.bind(this)
+        this.selectSrc = this.selectSrc.bind(this)
         this.state = {
-            number: "",
-            hex: "",
+            binary: "",
             octal: "",
-            binary: ""
+            decimal: "",
+            hex: "",
+            selected: "decimal"
         }
         this.types = {
-            "number-input": { read: numbers.strToInt, state: "number", write: numbers.intToStr },
-            "hex-input": { read: numbers.hexStrToInt, state: "hex", write: numbers.intToHexStr },
+            "binary-input": { read: numbers.binaryStrToInt, state: "binary", write: numbers.intToBinaryStr },
             "octal-input": { read: numbers.octalStrToInt, state: "octal", write: numbers.intToOctalStr },
-            "binary-input": { read: numbers.binaryStrToInt, state: "binary", write: numbers.intToBinaryStr }
+            "decimal-input": { read: numbers.strToInt, state: "decimal", write: numbers.intToStr },
+            "hex-input": { read: numbers.hexStrToInt, state: "hex", write: numbers.intToHexStr }
         }
     }
 
@@ -32,6 +41,7 @@ export default class Numbers extends React.Component {
         this.inputStream = new Bacon.Bus()
         const converted = this.inputStream
             .combine(inputConverter, (i, c) => c(i)).map((v) => (typeof(v) == "number" && !isNaN(v)) ? v : undefined)
+        this.selectedSrcStr = new Bacon.Bus()
         Object.keys(this.types).forEach((type) => {
             const typeInfo = this.types[type]
             const sourceIsThis = this.currentInput.map((id) => id == type)
@@ -40,6 +50,10 @@ export default class Numbers extends React.Component {
                 .map((v) => util.isString(v) ? v : "")
                 .onValue((v) => this.setState({[typeInfo.state]: v}))
         })
+        this.selectedSrcStr
+            .map(t => this.types[t].write)
+            .combine(converted, (c, v) => c(v))
+            .onValue(v => this.props.onValue && this.props.onValue(v))
     }
 
     inputChanged(event) {
@@ -50,19 +64,29 @@ export default class Numbers extends React.Component {
         this.inputStream.push(value)
     }
 
+    selectSrc(event) {
+        const src = this.types[event.target.id].state
+        this.setState({ selected: src })
+        this.selectedSrcStr.push(event.target.id)
+    }
+
     render() {
-        return <Section title="Numerot">
+        return <Section title="Numerot" subtitle={texts[this.state.selected]}>
             <Item name="Binääri">
-                <TextField type="number" id="binary-input" hintText="Binääri" maxLength="40" value={this.state.binary} onChange={this.inputChanged} />
+                <TextField type="number" id="binary-input" hintText={texts["binary"]} maxLength="40"
+                           value={this.state.binary} onChange={this.inputChanged} onFocus={this.selectSrc} />
             </Item>
             <Item name="Oktaali">
-                <TextField type="number" id="octal-input" hintText="Oktaali" maxLength="30" value={this.state.octal} onChange={this.inputChanged} />
+                <TextField type="number" id="octal-input" hintText={texts["octal"]} maxLength="30"
+                           value={this.state.octal} onChange={this.inputChanged} onFocus={this.selectSrc} />
             </Item>
             <Item name="Desimaali">
-                <TextField type="number" id="number-input" hintText="Desimaali" maxLength="30" value={this.state.number} onChange={this.inputChanged} />
+                <TextField type="number" id="decimal-input" hintText={texts["decimal"]} maxLength="30"
+                           value={this.state.decimal} onChange={this.inputChanged} onFocus={this.selectSrc} />
             </Item>
             <Item name="Heksa">
-                <TextField type="text" id="hex-input" hintText="Heksa" maxLength="30" value={this.state.hex} onChange={this.inputChanged} />
+                <TextField type="text" id="hex-input" hintText={texts["hex"]} maxLength="30"
+                           value={this.state.hex} onChange={this.inputChanged} onFocus={this.selectSrc} />
             </Item>
         </Section>
 
