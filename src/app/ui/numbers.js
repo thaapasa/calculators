6d77554b247d5ad1e4fe749539c8e1306af3d20d
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-25 18:17:39 +0200

    Refactored number conversion code

--- src/app/ui/numbers.js
+++ src/app/ui/numbers.js
@@ -14,87 +14,75 @@ const texts = {
     "char": "Merkki"
 }
 
+const types = {
+    "binary": { read: numbers.binaryStrToInt, write: numbers.intToBinaryStr, inputType: "number", maxLength: 50 },
+    "octal": { read: numbers.octalStrToInt, write: numbers.intToOctalStr, inputType: "number", maxLength: 40 },
+    "decimal": { read: numbers.strToInt, write: numbers.intToStr, inputType: "number", maxLength: 40 },
+    "hex": { read: numbers.hexStrToInt, write: numbers.intToHexStr, inputType: "text", maxLength: 30 },
+    "char": { read: numbers.charToInt, write: numbers.intToChar, inputType: "text", maxLength: 1 }
+}
+const typeKeys = Object.keys(types)
+
 export default class Numbers extends React.Component {
 
     constructor(props) {
         super(props)
         this.inputChanged = this.inputChanged.bind(this)
         this.selectSrc = this.selectSrc.bind(this)
-        this.state = {
-            binary: "",
-            octal: "",
-            decimal: "",
-            hex: "",
-            char: "",
-            selected: "decimal"
-        }
-        this.types = {
-            "binary-input": { read: numbers.binaryStrToInt, state: "binary", write: numbers.intToBinaryStr },
-            "octal-input": { read: numbers.octalStrToInt, state: "octal", write: numbers.intToOctalStr },
-            "decimal-input": { read: numbers.strToInt, state: "decimal", write: numbers.intToStr },
-            "hex-input": { read: numbers.hexStrToInt, state: "hex", write: numbers.intToHexStr },
-            "char-input": { read: numbers.charToInt, state: "char", write: numbers.intToChar }
-        }
+        this.state = { selected: "decimal" }
+        typeKeys.forEach(t => this.state[t] = "")
     }
 
     componentDidMount() {
         const emptyStream = Bacon.never()
         this.currentInput = new Bacon.Bus()
-        const inputConverter = this.currentInput.map((id) => this.types[id].read)
+        const inputConverter = this.currentInput.map(t => types[t].read)
         this.inputStream = new Bacon.Bus()
         const converted = this.inputStream
-            .combine(inputConverter, (i, c) => c(i)).map((v) => (typeof(v) == "number" && !isNaN(v)) ? v : undefined)
+            .combine(inputConverter, (i, c) => c(i)).map(v => (typeof(v) == "number" && !isNaN(v)) ? v : undefined)
         this.selectedSrcStr = new Bacon.Bus()
-        Object.keys(this.types).forEach((type) => {
-            const typeInfo = this.types[type]
-            const sourceIsThis = this.currentInput.map((id) => id == type)
+        typeKeys.forEach(t => {
+            const typeInfo = types[t]
+            const sourceIsThis = this.currentInput.map(name => t == name)
             converted.combine(sourceIsThis, (c, i) => [c, i]).flatMapLatest((v) => v[1] ? emptyStream : converted)
                 .map(typeInfo.write)
                 .map((v) => util.isString(v) ? v : "")
-                .onValue((v) => this.setState({[typeInfo.state]: v}))
+                .onValue((v) => this.setState({[t]: v}))
         })
         this.selectedSrcStr
-            .map(t => this.types[t].write)
+            .map(t => types[t].write)
             .combine(converted, (c, v) => c(v))
             .onValue(v => this.props.onValue && this.props.onValue(v))
     }
 
     inputChanged(event) {
-        const id = event.target.id
+        const name = event.target.name
         const value = event.target.value
-        this.setState({ [this.types[id].state]: value })
-        this.currentInput.push(id)
+        this.setState({ [name]: value })
+        this.currentInput.push(name)
         this.inputStream.push(value)
     }
 
     selectSrc(event) {
-        const src = this.types[event.target.id].state
+        const src = event.target.name
         this.setState({ selected: src })
-        this.selectedSrcStr.push(event.target.id)
+        this.selectedSrcStr.push(event.target.name)
     }
 
     render() {
         return <HalfSection title="Numerot" subtitle={texts[this.state.selected]}>
-            <Item name="Binääri">
-                <TextField type="number" id="binary-input" hintText={texts["binary"]} maxLength="40"
-                           value={this.state.binary} onChange={this.inputChanged} onFocus={this.selectSrc} />
-            </Item>
-            <Item name="Oktaali">
-                <TextField type="number" id="octal-input" hintText={texts["octal"]} maxLength="30"
-                           value={this.state.octal} onChange={this.inputChanged} onFocus={this.selectSrc} />
-            </Item>
-            <Item name="Desimaali">
-                <TextField type="number" id="decimal-input" hintText={texts["decimal"]} maxLength="30"
-                           value={this.state.decimal} onChange={this.inputChanged} onFocus={this.selectSrc} />
-            </Item>
-            <Item name="Heksa">
-                <TextField type="text" id="hex-input" hintText={texts["hex"]} maxLength="30"
-                           value={this.state.hex} onChange={this.inputChanged} onFocus={this.selectSrc} />
-            </Item>
-            <Item name="Merkki">
-                <TextField type="text" id="char-input" hintText={texts["char"]} maxLength="1"
-                           value={this.state.char} onChange={this.inputChanged} onFocus={this.selectSrc} />
-            </Item>
+            {
+                typeKeys.map(t => <Item name={texts[t]} key={`${t}-item`}>
+                    <TextField type={types[t].inputType}
+                               name={t}
+                               hintText={texts[t]}
+                               maxLength={types[t].maxLength}
+                               value={this.state[t]}
+                               onChange={this.inputChanged}
+                               onFocus={this.selectSrc}
+                               key={t} />
+                </Item>)
+            }
         </HalfSection>
 
     }
