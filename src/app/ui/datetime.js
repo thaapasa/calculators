39e88817f005ff6299135d9902f50e6f63f2dfc7
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2016-11-30 17:25:44 +0200

    Added local UTC offset. Refactored time conversions

--- src/app/ui/datetime.js
+++ src/app/ui/datetime.js
@@ -46,22 +46,23 @@ const texts = {
 
 
 const typeInfo = {
-    week: { reportValue: true },
+    week: { reportValue: true, inputStyle: styles.center },
     focused: { },
     selected: { },
-    iso8601: { reportValue: true },
-    iso8601utc: { reportValue: true },
-    nameDay: { reportValue: true },
+    iso8601: { read: v => moment(v, moment.ISO_8601), write: m => m.isValid() ? m.format() : "", src: "iso8601", reportValue: true, fullWidth: true },
+    iso8601utc: { read: v => moment(v, moment.ISO_8601), write: m => m.isValid() ? m.toISOString() : "", src: "iso8601utc", reportValue: true, fullWidth: true },
+    nameDay: { reportValue: true, inputStyle: styles.center },
     weekDay: { },
-    javaTime: { read: readJavaTime, src: "javaTime", reportValue: true, write: m => m.valueOf() },
-    unixTime: { read: readUnixTime, src: "unixTime", reportValue: true, write: m => m.unix() },
-    day: { read: strToInt, src: "value", write: m => pad(m.date(), 2), style: styles.len2, maxLength: 2 },
-    month: { read: v => strToInt(v) - 1, src: "value", write: m => pad(m.month() + 1, 2), style: styles.len2, maxLength: 2 },
-    year: { read: strToInt, src: "value", write: m => m.year(), style: styles.len4, maxLength: 4 },
-    hour: { read: strToInt, src: "value", write: m => pad(m.hour(), 2), style: styles.len2, maxLength: 2 },
-    minute: { read: strToInt, src: "value", write: m => pad(m.minute(), 2), style: styles.len2, maxLength: 2 },
-    second: { read: strToInt, src: "value", write: m => pad(m.second(), 2), style: styles.len2, maxLength: 2 },
-    millisecond: { read: strToInt, src: "value", write: m => pad(m.millisecond(), 3), style: styles.len3, maxLength: 3 },
+    javaTime: { read: readJavaTime, src: "javaTime", reportValue: true, write: m => m.isValid() ? m.valueOf() : "", fullWidth: true },
+    unixTime: { read: readUnixTime, src: "unixTime", reportValue: true, write: m => m.isValid() ? m.unix() : "", fullWidth: true },
+    day: { read: strToInt, src: "value", write: m => pad(m.date(), 2), style: styles.len2, maxLength: 2, inputStyle: styles.center },
+    month: { read: v => strToInt(v) - 1, src: "value", write: m => pad(m.month() + 1, 2), style: styles.len2, maxLength: 2, inputStyle: styles.center },
+    year: { read: strToInt, src: "value", write: m => m.year(), style: styles.len4, maxLength: 4, inputStyle: styles.center },
+    hour: { read: strToInt, src: "value", write: m => pad(m.hour(), 2), style: styles.len2, maxLength: 2, inputStyle: styles.center },
+    minute: { read: strToInt, src: "value", write: m => pad(m.minute(), 2), style: styles.len2, maxLength: 2, inputStyle: styles.center },
+    second: { read: strToInt, src: "value", write: m => pad(m.second(), 2), style: styles.len2, maxLength: 2, inputStyle: styles.center },
+    millisecond: { read: strToInt, src: "value", write: m => pad(m.millisecond(), 3), style: styles.len3, maxLength: 3, inputStyle: styles.center },
+    timeZone: { write: m => m.isValid() ? m.format('Z') : "", style: styles.len7, maxLength: 6, inputStyle: styles.center, readOnly: true },
     direct: { src: "direct" }
 }
 
@@ -72,7 +73,9 @@ const hints = {
     hour: "10",
     minute: "00",
     second: "00",
-    millisecond: "000"
+    millisecond: "000",
+    timeZone: "+02:00"
+
 }
 
 const valueTypes = ["day", "month", "year", "hour", "minute", "second", "millisecond"]
@@ -116,6 +119,7 @@ export default class DateTime extends React.Component {
             millisecond: incoming.millisecond
         }).map(v => moment(v))
         const newVal = Bacon.mergeAll(incoming.direct, incoming.unixTime, incoming.javaTime,
+            incoming.iso8601, incoming.iso8601utc,
             Bacon.combineAsArray(incoming.value, this.streams.selected)
                 .flatMapLatest(t => t[1] == "value" ? t[0] : Bacon.never()))
         Bacon.combineAsArray(newVal, this.streams.selected).onValue(r => {
@@ -128,8 +132,6 @@ export default class DateTime extends React.Component {
                 if (src != "value" && valueTypes.includes(t))
                     this.streams[t].push(output)
             })
-            incoming.iso8601.push(toStateValue(val, v => v.isValid() && v.format()))
-            incoming.iso8601utc.push(toStateValue(val, v => v.isValid() && v.utc().format()))
             incoming.week.push(toStateValue(val, toIsoWeek))
             incoming.weekDay.push(toStateValue(val, v => texts.weekDay[v.isoWeekday()]))
             incoming.nameDay.push(toStateValue(val, v => getNameDay(v.month() + 1, v.date())))
@@ -154,21 +156,20 @@ export default class DateTime extends React.Component {
     }
 
     renderType(type) {
-        return <TextField type="text" value={this.state[type]} style={typeInfo[type].style}
-                          maxLength={typeInfo[type].maxLength} name={type} hintText={hints[type]}
-                          inputStyle={styles.center} hintStyle={styles.center}
-                          onChange={this.inputChanged} onFocus={this.focusChanged} />
+        const info = typeInfo[type]
+        return <TextField type="text" value={this.state[type]} style={info.style}
+                          maxLength={info.maxLength} name={type} hintText={hints[type]}
+                          inputStyle={info.inputStyle} hintStyle={info.inputStyle} fullWidth={info.fullWidth}
+                          onChange={this.inputChanged} onFocus={this.focusChanged} readOnly={info.readOnly} />
     }
 
     render() {
         return <HalfSection title="Aikaleimat">
             <Item name="Java/JS time">
-                <TextField type="text" value={this.state.javaTime} fullWidth={true} name="javaTime"
-                           onChange={this.inputChanged } onFocus={this.focusChanged} />
+                { this.renderType("javaTime") }
             </Item>
             <Item name="Unixtime">
-                <TextField type="text" value={this.state.unixTime} fullWidth={true} name="unixTime"
-                           onChange={this.inputChanged} onFocus={this.focusChanged} />
+                { this.renderType("unixTime") }
             </Item>
             <Item name="Päivä" style={styles.item}>
                 {this.renderType("day")}.{this.renderType("month")}.{this.renderType("year")}
@@ -177,7 +178,7 @@ export default class DateTime extends React.Component {
                             onFocus={this.focusChanged} />)
             </Item>
             <Item name="Kellonaika" style={styles.item}>
-                {this.renderType("hour")}:{this.renderType("minute")}:{this.renderType("second")}.{this.renderType("millisecond")}
+                {this.renderType("hour")}:{this.renderType("minute")}:{this.renderType("second")}.{this.renderType("millisecond")} {this.renderType("timeZone")}
             </Item>
             <Item name="Viikko">
                 <TextField type="text" name="week" value={this.state.week} style={styles.len7} readOnly
@@ -189,12 +190,10 @@ export default class DateTime extends React.Component {
                            multiLine={true} onFocus={this.focusChanged} />
             </Item>
             <Item name="ISO-8601">
-                <TextField type="text" name="iso8601" value={this.state.iso8601} fullWidth={true} readOnly
-                           onFocus={this.focusChanged} />
+                { this.renderType("iso8601") }
             </Item>
             <Item name="ISO-8601 UTC">
-                <TextField type="text" name="iso8601utc" value={this.state.iso8601utc} fullWidth={true} readOnly
-                           onFocus={this.focusChanged} />
+                { this.renderType("iso8601utc") }
             </Item>
             </HalfSection>
 
