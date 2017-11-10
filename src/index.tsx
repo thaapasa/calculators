5575b0a28adaa9425cbacb90b3b598041cde1bef
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-11-10 21:19:47 +0200

    Remove tap event plugin

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,11 @@ import ReactDOM from "react-dom"
 import CalculatorPage from "./app/ui/page"
 import {fixConsole} from "./app/util/util"
 import {addBaconSafeLog,addBaconSetStateValue} from "./app/util/baconutil"
-import injectTapEventPlugin from "react-tap-event-plugin"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 
 function init() {
     // Ensure that there is a console
     fixConsole()
-    // Needed for onTouchTap
-    // http://stackoverflow.com/a/34015469/988941
-    injectTapEventPlugin()
 
     addBaconSafeLog()
     addBaconSetStateValue()
