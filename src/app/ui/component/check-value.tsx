5f67740ae5ae34adb7662e2621a09a480caed5a5
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-11-14 21:07:01 +0200

    Add type info to check-value

--- src/app/ui/component/check-value.tsx
+++ src/app/ui/component/check-value.tsx
@@ -1,13 +1,13 @@
-import * as React from "react"
-import * as Bacon from "baconjs"
-import { GenerateButton } from "./tool-button"
-import * as util from "../../util/util"
-import Item from "./item"
-import TextField from "material-ui/TextField"
+import * as React from 'react';
+import * as Bacon from 'baconjs';
+import { GenerateButton } from './tool-button';
+import * as util from '../../util/util';
+import Item from './item';
+import TextField from 'material-ui/TextField';
 
 const styles = {
-    check: { width: "1em" },
-    itemValue: { alignItems: "flex-start" }
+    check: { width: '1em' },
+    itemValue: { alignItems: 'flex-start' },
 }
 
 interface CheckProps {
@@ -29,67 +29,59 @@ interface CheckState {
 
 export default class CheckValue extends React.Component<CheckProps, CheckState> {
 
-    private check: any
-
     public state: CheckState = {
-        input: "",
-        value: "",
-        checkValue: ""
+        input: '',
+        value: '',
+        checkValue: ''
     };
 
     private inputStyle = {
-        width: ""
+        width: ''
     };
 
-    private inputStream: any = null
+    private inputStream: Bacon.Bus<any, string>
 
-    constructor(props: CheckProps) {
+    public constructor(props: CheckProps) {
         super(props)
         if (this.props.width)
             this.inputStyle.width = this.props.width
-
-        this.generate = this.generate.bind(this)
-        this.inputChanged = this.inputChanged.bind(this)
     }
 
-    componentDidMount() {
+    public componentDidMount() {
         this.streamToCheck(this.props.check, this.props.combine)
     }
 
-    generate() {
+    private generate = () => {
         const generated: string = this.props.generate().toString()
         this.setState({ input: generated })
         this.inputStream.push(generated)
     }
 
-    inputChanged(event: any) {
-        const inp = event.target.value
-        this.setState({ input: inp })
-        this.inputStream.push(inp)
+    private inputChanged = (_: any, val: string) => {
+        this.setState({ input: val })
+        this.inputStream.push(val)
     }
 
-    streamToCheck(calculateCheck: any, combiner = util.combineWith("")) {
-        this.inputStream = new Bacon.Bus()
+    private streamToCheck(calculateCheck: (x: string) => string, combiner: (a: string, b: string) => string = util.combineWith('')) {
+        this.inputStream = new Bacon.Bus<any, string>()
         const checkValue = this.inputStream.map(calculateCheck)
-        checkValue.onValue((value: any) => {
-            this.setState({ checkValue: (value !== undefined) ? value : "" })
-        })
+        checkValue.onValue(checkValue => this.setState({ checkValue }))
         checkValue
-            .combine(this.inputStream, (chk: any, inp: any) => (chk !== undefined) && combiner(inp, chk))
+            .combine(this.inputStream.toProperty(''), (chk, inp) => chk && combiner(inp, chk))
             .filter(util.nonEmpty)
-            .onValue((v: any) => {
-                this.setState({ value: v || "" })
+            .onValue(v => {
+                this.setState({ value: v || '' })
                 this.props.onValue && this.props.onValue(v)
             })
     }
 
-    render() {
+    public render() {
         return <Item name={this.props.name} valueStyle={styles.itemValue}>
             <GenerateButton onClick={this.generate} title="Luo uusi" />
             <TextField type="text" id={`${this.props.id}-input`} onChange={this.inputChanged}
-                   style={this.inputStyle} value={this.state.input} max-length={this.props['max-length']} />
-            <TextField id={`${this.props.id}-check`} ref={(i) => this.check = i}
-                   className="letter" read-only='read-only' value={this.state.checkValue} style={styles.check} />
+                style={this.inputStyle} value={this.state.input} max-length={this.props['max-length']} />
+            <TextField id={`${this.props.id}-check`}
+                className="letter" read-only='read-only' value={this.state.checkValue} style={styles.check} />
             <input type="hidden" id={`${this.props.id}-value`} value={this.state.value} />
         </Item>
     }
