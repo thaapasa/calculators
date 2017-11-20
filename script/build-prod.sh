a1f1f8ef182abcd5ef1838cfd1a1547dad48daea
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-11-20 18:21:13 +0200

    Tuned prod build script

--- script/build-prod.sh
+++ script/build-prod.sh
@@ -3,9 +3,12 @@
 pushd . >/dev/null
 cd `dirname $0`/..
 
+echo 'Installing dependencies...'
 npm install
 
-npm run build && \
-    echo "Build successful!"
+echo 'Building production package...'
+npm run build || exit -1
+
+echo "Build successful!"
 
 popd >/dev/null
