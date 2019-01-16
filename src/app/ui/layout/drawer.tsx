8ebc057e8652815c69fed33801bf6f8ee3283b89
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-01-16 21:23:43 +0200

    Update favicon

--- src/app/ui/layout/drawer.tsx
+++ src/app/ui/layout/drawer.tsx
@@ -13,17 +13,27 @@ export default class NavigationDrawer extends React.Component<NavigationProps> {
     public render() {
         return (
             <Drawer open={this.props.open} onRequestChange={this.props.onToggle} docked={false}>
-                <Card>
-                    <CardHeader title="Laskurit" subtitle={`v. ${ver.version}`} avatar="img/calculators.png" />
-                </Card>
-                <MenuItem onClick={this.goToFullpage}>Kaikki</MenuItem>
-                <MenuItem onClick={this.goToNumbers}>Numerot ja merkit</MenuItem>
-                <MenuItem onClick={this.goToTime}>Aikaleimat</MenuItem>
-                <MenuItem onClick={this.goToIdentifiers}>Tunnisteet</MenuItem>
-                <MenuItem onClick={this.goToColors}>Värit</MenuItem>
-                <MenuItem onClick={this.goToLinks}>Linkit</MenuItem>
-                <MenuItem onClick={this.goToTextConversion}>Tekstimuunnokset</MenuItem>
-                <MenuItem onClick={this.goToCryptography}>Kryptografia</MenuItem>
+                <div className="flex-column">
+                    <Card>
+                        <CardHeader title="Laskurit" subtitle={`v. ${ver.version}`} avatar="img/calculators.png" />
+                    </Card>
+                    <MenuItem onClick={this.goToFullpage}>Kaikki</MenuItem>
+                    <MenuItem onClick={this.goToNumbers}>Numerot ja merkit</MenuItem>
+                    <MenuItem onClick={this.goToTime}>Aikaleimat</MenuItem>
+                    <MenuItem onClick={this.goToIdentifiers}>Tunnisteet</MenuItem>
+                    <MenuItem onClick={this.goToColors}>Värit</MenuItem>
+                    <MenuItem onClick={this.goToLinks}>Linkit</MenuItem>
+                    <MenuItem onClick={this.goToTextConversion}>Tekstimuunnokset</MenuItem>
+                    <MenuItem onClick={this.goToCryptography}>Kryptografia</MenuItem>
+                    <div className="flex" />
+                    <div className="license-info">
+                        <div>Calculator icon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a>{' '}
+                            from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>{' '}
+                            is licensed by{' '}
+                            <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+                        </div>
+                    </div>
+                </div>
             </Drawer>
         )
     }
