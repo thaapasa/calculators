6ae6bd5758f1f822ca6789d29ef344c21be9a91c
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-02-01 13:42:37 +0200

    Revert "Restore original theme"
    This reverts commit 5df70e8bfcdc73164c87f5fbe891b952d6165460.

--- src/style.ts
+++ src/style.ts
@@ -1,13 +1,10 @@
+import { blue, pink } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: 'rgb(232, 232, 232)',
-    },
-    secondary: {
-      main: 'rgb(244, 67, 54)',
-    },
+    primary: blue,
+    secondary: pink,
   },
   typography: {
     useNextVariants: true,
