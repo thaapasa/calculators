baacd7220c6f90e27a0e405f21eefa9fcaf0be51
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-02-26 09:39:53 +0200

    Clean up color bar component

--- src/app/ui/component/color-bar.tsx
+++ src/app/ui/component/color-bar.tsx
@@ -1,6 +1,5 @@
 import { rgbToHex, RGBValue } from 'app/calc/colors';
 import React from 'react';
-import { Size, withSize } from 'react-sizeme';
 import styled from 'styled-components';
 
 interface ColorBarProps {
@@ -9,32 +8,28 @@ interface ColorBarProps {
 }
 
 export class ColorBar extends React.Component<ColorBarProps> {
-  public render() {
-    return (
-      <Bar className={this.props.className}>
-        <SizedInnerBar colors={this.props.colors} />
-      </Bar>
-    );
-  }
-  foo = (s: Size) => console.log(s);
-}
-
-class InnerBar extends React.Component<{
-  size: Size;
-  colors: RGBValue[];
-}> {
   private canvas = React.createRef<HTMLCanvasElement>();
+
   public componentDidUpdate() {
     this.updateCanvas();
   }
+
   public componentDidMount() {
     this.updateCanvas();
   }
+
   public render() {
     return (
-      <Canvas ref={this.canvas} width={this.props.colors.length} height={10} />
+      <Bar className={this.props.className}>
+        <Canvas
+          ref={this.canvas}
+          width={this.props.colors.length}
+          height={10}
+        />
+      </Bar>
     );
   }
+
   private updateCanvas() {
     const canvas = this.canvas.current;
     if (!canvas) {
@@ -50,8 +45,6 @@ class InnerBar extends React.Component<{
     });
   }
 }
-
-export const SizedInnerBar = withSize({ monitorWidth: true })(InnerBar);
 
 const Bar = styled.div`
   height: 24px;
