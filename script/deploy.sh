92da24b7c46fd3d76567979e9997761e3615adbe
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2020-07-04 15:03:44 +0300

    Update deploy script to target full host name

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,16 +3,18 @@
 pushd . >/dev/null
 cd `dirname $0`/..
 
+HOST=laskurit.pomeranssi.fi
+
 export REV=`git rev-parse HEAD | cut -c 1-8`
 script/build-prod.sh
 
 echo "Copying files to production (rev $REV)..."
 
-ssh deployer@pomeranssi.fi "mkdir -p calculators/deploy" || exit -1
-scp deploy/calculators-$REV.tar.gz deployer@pomeranssi.fi:~/calculators/deploy || exit -1
+ssh deployer@$HOST "mkdir -p calculators/deploy" || exit -1
+scp deploy/calculators-$REV.tar.gz deployer@$HOST:~/calculators/deploy || exit -1
 
 echo "Deploying on server..."
 
-ssh deployer@pomeranssi.fi "bash --login -c 'cd ~/calculators && git pull && script/install-prod.sh $REV'"
+ssh deployer@$HOST "bash --login -c 'cd ~/calculators && git pull && script/install-prod.sh $REV'"
 
 popd >/dev/null
